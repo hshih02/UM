ca902254b0bcfdb8b14b3a51b91a4f2889dc2081
Ryan Hoff <fhoff01@vm-hw06.eecs.tufts.edu>
2019-04-11 19:13:17 -0400

    added tests

--- umtests/umlabwrite.c
+++ umtests/umlabwrite.c
@@ -31,9 +31,12 @@ static struct test_info {
         { "halt-verbose", NULL, "", emit_verbose_halt_test },
         { "add",          NULL, "", emit_add_test},
         { "print-six",    NULL, "6", emit_print_six_test},
-        { "output",       NULL, "132", emit_output_test},
-        { "cmov",         NULL, "232", emit_cmov_test},
-        { "sload-sstore", NULL, "187904819277277", emit_sstore_sload_test}
+        { "output",       NULL, ":-)", emit_output_test},
+        { "cmov",         NULL, "O_O", emit_cmov_test},
+        { "sload-sstore", NULL, "MM", emit_sstore_sload_test}, /* 77 2 77 */
+        { "mult",         NULL, "<", emit_multiply_test}
+        { "div",          NULL, "d", emit_divide_test}
+        { "nand",         NULL, "i", emit_nand_test}
 };
 
   
